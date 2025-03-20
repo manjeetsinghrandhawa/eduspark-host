f624b3ca5dd545a98e2837fec3a03b143ab5f5d5
manjeetsinghrandhawa <manjeetsinghh052003@gmail.com>
2025-03-20 12:32:27 +0530

    server changed

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:["http://localhost:3000", "https://eduspark-git-main-manjeet-singhs-projects-803e5245.vercel.app/"],
 		credentials:true,
 	})
 )
